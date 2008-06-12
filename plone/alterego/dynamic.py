3e2e98248fb433d4b0dce36d434296e6bb6d0707
Martin Aspeli <optilude@gmail.com>
2008-06-12 21:45:01 +0000

    Simplify the dynamic module - we only want to do a dynamic lookup if the attribute does not exist, and we want to make it easier to set the attribute (via setattr()).
    svn path=/plone.alterego/trunk/; revision=21177

--- plone/alterego/dynamic.py
+++ plone/alterego/dynamic.py
@@ -13,23 +13,20 @@ class DynamicModule(module):
     
     implements(IDynamicModule)
 
-    def __getattribute__(self, name):
-        try:
-            return module.__getattribute__(self, name)
-        except AttributeError, e:
-            
-            __name__ = module.__getattribute__(self, '__name__')
-            factory = queryUtility(IDynamicObjectFactory, name=__name__)
-            if factory is None:
-                raise AttributeError("Cannot find dynamic object factory for module %s" % __name__)
-            
-            obj = factory(name, self)
-            if obj is None:
-                raise e
-            
-            __dict__ = module.__getattribute__(self, '__dict__')
-            __dict__[name] = obj
-            return obj
+    def __getattr__(self, name):
+        
+        if name == '__path__':
+            raise AttributeError("Dynamic modules do not have __path__'s")
+        
+        factory = queryUtility(IDynamicObjectFactory, name=self.__name__)
+        if factory is None:
+            raise AttributeError("Cannot find dynamic object factory for module %s" % self.__name__)
+        
+        obj = factory(name, self)
+        if obj is None:
+            raise e
+
+        return obj
             
 def create(dotted_name):
     dynamic = DynamicModule(dotted_name)
