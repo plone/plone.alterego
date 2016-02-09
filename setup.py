c80cee46e5659d3a41bb4ed268949d1fe1fcdab4
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:13:54 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,31 +3,36 @@ import os
 
 version = '1.0.1'
 
-setup(name='plone.alterego',
-      version=version,
-      description="Low level support for dynamic modules",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.alterego',
+    version=version,
+    description="Low level support for dynamic modules",
+    long_description=(open("README.txt").read() + "\n" +
+                      open(os.path.join("docs", "HISTORY.txt")).read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.6',
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='Plone schema interface',
-      author='Laurence Rowe',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://code.google.com/p/dexterity',
-      license='LGPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='Plone schema interface',
+    author='Laurence Rowe',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/plone.alterego',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
