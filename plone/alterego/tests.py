60c3a62d3accd9cd46e660a0e4e79e9b5860fab2
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-01 23:27:29 +0200

    feat: drop python 2 code paths

--- plone/alterego/tests.py
+++ plone/alterego/tests.py
@@ -1,21 +1,6 @@
 import doctest
-import sys
 import unittest
 import zope.component.testing
-
-
-SKIP_PYTHON_2 = doctest.register_optionflag("SKIP_PYTHON_2")
-SKIP_PYTHON_3 = doctest.register_optionflag("SKIP_PYTHON_3")
-
-
-class PolyglotOutputChecker(doctest.OutputChecker):
-    def check_output(self, want, got, optionflags):
-        if optionflags & SKIP_PYTHON_3 and sys.version_info >= (3,):
-            return True
-        elif optionflags & SKIP_PYTHON_2:
-            return True
-
-        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 def test_suite():
@@ -24,7 +9,6 @@ def test_suite():
             doctest.DocFileSuite(
                 "alterego.txt",
                 tearDown=zope.component.testing.tearDown,
-                checker=PolyglotOutputChecker(),
             ),
         )
     )
